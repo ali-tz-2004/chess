f1a104d7b9bc0d5cabe4a1e1781c214ff4c1b4bd
alitz <alitz1382@gmail.com>
2024-12-10 10:39:12 +0330

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#212121",
+        secondary: "#2F2F2F",
+        cellPrimary: "#000",
+        cellSecondary: "#fff"
+      },
+      height:{
+        "custom-30": "30rem"
+      },
+      width:{
+        "custom-30": "30rem"
+      },
+      gridRow:{
+        8:"grid-template-rows: repeat(8, minmax(0, 1fr));"
+      }
+    },
+  },
+  plugins: [],
+}
+

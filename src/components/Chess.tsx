0ea1fb4b0423617ff8085a8161339c49395e67d9
alitz <alitz1382@gmail.com>
2024-12-16 13:47:22 +0330

    add mode white pawn

--- src/components/Chess.tsx
+++ src/components/Chess.tsx
@@ -2,13 +2,12 @@ import { useEffect, useState } from "react";
 import { BorderChess } from "./BorderChess";
 import { ChessPiece } from "./ChessPiece";
 import { Images } from "../assets/images/images";
-import Draggable from "react-draggable";
 
 type cellColorType = "bg-cellPrimary" | "bg-cellSecondary";
 
 interface CellType {
   id: number;
-  color: cellColorType;
+  bgColor: cellColorType;
   piece: keyof (typeof Images)["black"] | null;
   colorPiece: keyof typeof Images | null;
   selected: boolean;
@@ -67,11 +66,41 @@ export const Chess = () => {
         case "black":
           break;
         case "white":
-          if (firstIndex === 6) {
-            cell[firstIndex - 1][secondIndex].isAllowMove =
-              cellSelected.selected;
-            cell[firstIndex - 2][secondIndex].isAllowMove =
-              cellSelected.selected;
+          switch (cellSelected.piece) {
+            case "Pawn":
+              if (
+                firstIndex === 6 &&
+                !cell[firstIndex - 1][secondIndex].piece &&
+                !cell[firstIndex - 2][secondIndex].piece
+              ) {
+                cell[firstIndex - 1][secondIndex].isAllowMove =
+                  cellSelected.selected;
+                cell[firstIndex - 2][secondIndex].isAllowMove =
+                  cellSelected.selected;
+              }
+              if (!cell[firstIndex - 1][secondIndex].piece) {
+                cell[firstIndex - 1][secondIndex].isAllowMove =
+                  cellSelected.selected;
+              }
+              if (
+                secondIndex != 0 &&
+                cell[firstIndex - 1][secondIndex - 1].piece &&
+                cell[firstIndex - 1][secondIndex - 1].colorPiece === "black"
+              ) {
+                cell[firstIndex - 1][secondIndex - 1].isAllowMove =
+                  cellSelected.selected;
+              }
+              if (
+                secondIndex != 7 &&
+                cell[firstIndex - 1][secondIndex + 1].piece &&
+                cell[firstIndex - 1][secondIndex + 1].colorPiece === "black"
+              ) {
+                cell[firstIndex - 1][secondIndex + 1].isAllowMove =
+                  cellSelected.selected;
+              }
+              break;
+            default:
+              break;
           }
           break;
       }
@@ -96,7 +125,7 @@ export const Chess = () => {
 
         let temp: CellType = {
           id: id,
-          color: color,
+          bgColor: color,
           piece: piece,
           colorPiece: colorPiece,
           selected: false,
@@ -111,20 +140,30 @@ export const Chess = () => {
     setCells(tempList);
   }, []);
 
+  const movedHandler = (
+    isAllowMove: boolean,
+    firstIndex: number,
+    secondIndex: number
+  ): void => {
+    if (isAllowMove) {
+    }
+  };
+
   return (
     <div
       className="h-custom-30 w-custom-30 bg-secondary grid grid-cols-8 grid-rows-8 p-8 relative
-    2xl:scale-150 lg:scale-100 md:scale-50 scale-75 "
+    2xl:scale-150 lg:scale-100 md:scale-50 scale-75"
     >
       {cells.map((cellChildren, firstIndex) =>
         cellChildren.map((x, secondIndex) => (
           <div
             key={x.id}
             className={`${
-              x.color
-            } w-full h-full z-10 relative flex items-center justify-center ${
+              x.bgColor
+            } w-full h-full z-10 relative flex items-center justify-center  ${
               x.isAllowMove ? "cursor-pointer" : ""
             } `}
+            onClick={() => movedHandler(x.isAllowMove, firstIndex, secondIndex)}
           >
             {x.isAllowMove ? (
               <div className="absolute w-2 h-2 bg-blue-400 rounded-xl"></div>
